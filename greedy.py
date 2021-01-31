09dd1f89373137fe2fac0c4c272f1f3149cfc136
Jasmin Sternkopf <jasmin_sternkopf@web.de>
2021-01-31 03:44:32 +0100

    adds first tests, small changes

--- greedy.py
+++ greedy.py
@@ -1,47 +1,52 @@
+from collections import Counter
+from typing import Dict, List, Tuple
+
 import numpy as np
 
-from Typing import Tuple, Dict, List
 
-def greedy(target_dist: Dict[Tuple[str,str], float], selection_list: List[List[Tuple[str, str]]], max_iter: int) -> List[List[Tuple[str, str]]]:
-    cover: List[List[Tuple[str, str]]] = []
-    for _ in range(max_iter):
-        best_sentence = find_sentence_with_min_div(selection_list, cover, target_dist)
-        cover.append(best_sentence)
-        selection_list.remove(best_sentence)
-    return cover
+def greedy(target_dist: Dict[Tuple[str, str], float], selection_list: List[List[Tuple[str, str]]], max_iter: int) -> List[List[Tuple[str, str]]]:
+  cover: List[List[Tuple[str, str]]] = []
+  for _ in range(max_iter):
+    best_sentence = find_sentence_with_min_div(selection_list, cover, target_dist)
+    cover.append(best_sentence)
+    selection_list.remove(best_sentence)
+  return cover
+
 
 def find_sentence_with_min_div(selection_list, cover, target_dist) -> List[Tuple[str, str]]:
-    min_div = get_divergence_for_sentence(selection_list[0], cover, target_dist)
-    best_sentence = selection_list[0]
-    for sentence in selection_list[1:]:
-        if get_divergence_for_sentence(sentence, cover, target_dist) < min_div:
-            best_sentence = sentence
-    return best_sentence
+  min_div = get_divergence_for_sentence(selection_list[0], cover, target_dist)
+  best_sentence = selection_list[0]
+  for sentence in selection_list[1:]:
+    if get_divergence_for_sentence(sentence, cover, target_dist) < min_div:
+      best_sentence = sentence
+  return best_sentence
+
 
 def get_divergence_for_sentence(sentence, cover, target_dist) -> float:
-    cover = cover.append(sentence)
-    cover_dist = get_distribution(cover, target_dist)
-    divergence = kullback_leibler_div(cover_dist, target_dist)
-    return divergence
+  cover = cover.append(sentence)
+  cover_dist = get_distribution(cover, target_dist)
+  divergence = kullback_leibler_div(cover_dist, target_dist)
+  return divergence
 
-def kullback_leibler_div(dist_1: Dict[Tuple[str,str], float], dist_2: Dict[Tuple[str,str], float]) -> float:
-    for value in dist_2.values():
-        assert value > 0
-    unequal_zero_keys = [key for key in dist_1.keys() if dist_1[key] > 0]
-    divergence = [dist_1[key]*(np.log(dist_1[key])-np.log(dist_2[key])) for key in unequal_zero_keys].sum()
-    return divergence
 
-def get_distribution(sentence_list: List[List[Tuple[str, str]]], target_dist: Dict[Tuple[str,str], float]) -> Dict[Tuple[str,str], float]:
-    new_dist = {key: count_occurrences_of_unit_in_list_of_sentences(key, sentence_list) for key in target_dist.keys()}
-    total_number_of_single_units = new_dist.values().sum()
-    for value in new_dist.values():
-        value = value / total_number_of_single_units
-    return new_dist
+def kullback_leibler_div(dist_1: Dict[Tuple[str, str], float], dist_2: Dict[Tuple[str, str], float]) -> float:
+  for value in dist_2.values():
+    assert value > 0
+  unequal_zero_keys = [key for key in dist_1.keys() if dist_1[key] > 0]
+  divergence = [dist_1[key] * (np.log(dist_1[key]) - np.log(dist_2[key]))
+                for key in unequal_zero_keys]
+  return sum(divergence)
+
+
+def get_distribution(sentence_list: List[List[Tuple[str, str]]], target_dist: Dict[Tuple[str, str], float]) -> Dict[Tuple[str, str], float]:
+  new_dist = {key: count_occurrences_of_unit_in_list_of_sentences(
+    key, sentence_list) for key in target_dist.keys()}
+  total_number_of_single_units = sum(new_dist.values())
+  for key, value in new_dist.items():
+    new_dist[key] = value / total_number_of_single_units
+  return new_dist
+
 
 def count_occurrences_of_unit_in_list_of_sentences(unit: Tuple[str, str], sentence_list: List[List[Tuple[str, str]]]) -> int:
-    occurence_list = [count_occurrences_of_unit_in_one_sentence(unit, sentence) for sentence in sentence_list]
-    return occurence_list.sum()
-
-def count_occurrences_of_unit_in_one_sentence(unit: Tuple[str, str], sentence: List[Tuple[str, str]]) -> int:
-    element_is_unit = [sentence_unit == unit for sentence_unit in sentence]
-    return element_is_unit.sum()
+  occurence_list = [Counter(sentence)[unit] for sentence in sentence_list]
+  return sum(occurence_list)
--- greedy.tests.py
+++ greedy.tests.py
@@ -1,6 +1,50 @@
-from greedy import greedy, find_sentence_with_min_div, get_distribution, get_divergence_for_sentence, count_occurrences_of_unit_in_list_of_sentences, count_occurrences_of_unit_in_one_sentence
 import unittest
+
+from greedy import (count_occurrences_of_unit_in_list_of_sentences,
+                    find_sentence_with_min_div, get_distribution,
+                    get_divergence_for_sentence, greedy)
+
 
 class UnitTests(unittest.TestCase):
   def __init__(self, methodName: str) -> None:
     super().__init__(methodName)
+
+  # region count_occurrences_of_unit_in_list_of_sentences
+
+  def test_count_occurrences_of_unit_in_list_of_sentences_du_3(self):
+    test_list = [[("Hallo", "h"), ("du", "d"), ("und", "u"), ("du", "d")],
+                 [("Bye", "b"), ("und", "u"), ("du", "d")]]
+    res = count_occurrences_of_unit_in_list_of_sentences(("du", "d"), test_list)
+
+    self.assertEqual(3, res)
+
+  def test_count_occurrences_of_unit_in_list_of_sentences_Hallo_1(self):
+    test_list = [[("Hallo", "h"), ("du", "d"), ("und", "u"), ("du", "d")],
+                 [("Bye", "b"), ("und", "u"), ("du", "d")]]
+    res = count_occurrences_of_unit_in_list_of_sentences(("Hallo", "h"), test_list)
+
+    self.assertEqual(1, res)
+
+  # endregion
+
+  # region get_distribuion
+
+  def test_get_distribution(self):
+    test_list = [[("Hallo", "h"), ("du", "d"), ("und", "u"), ("du", "d")],
+                 [("Bye", "b"), ("und", "u"), ("du", "d")]]
+    target_dist = {("Hallo", "h"): 0.2, ("du", "d"): 0.3, ("und", "u"): 0.4, ("Bye", "b"): 0.1}
+    res = get_distribution(test_list, target_dist)
+    right_dist = {("Hallo", "h"): 1 / 7, ("du", "d"): 3 / 7,
+                  ("und", "u"): 2 / 7, ("Bye", "b"): 1 / 7}
+
+    self.assertEqual(len(res), len(right_dist))
+
+    for key in res.keys():
+      self.assertAlmostEqual(res[key], right_dist[key])
+
+  # endregion
+
+  # region get_divergence_for_sentence
+if __name__ == '__main__':
+  suite = unittest.TestLoader().loadTestsFromTestCase(UnitTests)
+  unittest.TextTestRunner(verbosity=2).run(suite)
