7da455a37bbc85c8ccafc0de7ebf2685fefd2162
Jasmin Sternkopf <jasmin_sternkopf@web.de>
2021-01-31 06:07:35 +0100

    adds testcases

--- greedy.py
+++ greedy.py
@@ -7,24 +7,29 @@ import numpy as np
 def greedy(target_dist: Dict[Tuple[str, str], float], selection_list: List[List[Tuple[str, str]]], max_iter: int) -> List[List[Tuple[str, str]]]:
   cover: List[List[Tuple[str, str]]] = []
   for _ in range(max_iter):
-    best_sentence = find_sentence_with_min_div(selection_list, cover, target_dist)
+    current_cover = cover.copy()
+    best_sentence = find_sentence_with_min_div(selection_list, current_cover, target_dist)
     cover.append(best_sentence)
     selection_list.remove(best_sentence)
   return cover
 
 
-def find_sentence_with_min_div(selection_list, cover, target_dist) -> List[Tuple[str, str]]:
-  min_div = get_divergence_for_sentence(selection_list[0], cover, target_dist)
+def find_sentence_with_min_div(selection_list, current_cover, target_dist) -> List[Tuple[str, str]]:
+  curr_cover = current_cover.copy()
+  min_div = get_divergence_for_sentence(selection_list[0], curr_cover, target_dist)
   best_sentence = selection_list[0]
   for sentence in selection_list[1:]:
-    if get_divergence_for_sentence(sentence, cover, target_dist) < min_div:
+    curr_cover = current_cover.copy()
+    new_div = get_divergence_for_sentence(sentence, curr_cover, target_dist)
+    if new_div < min_div:
       best_sentence = sentence
+      min_div = new_div
   return best_sentence
 
 
-def get_divergence_for_sentence(sentence, cover, target_dist) -> float:
-  cover = cover.append(sentence)
-  cover_dist = get_distribution(cover, target_dist)
+def get_divergence_for_sentence(sentence, current_cover, target_dist) -> float:
+  current_cover.append(sentence)
+  cover_dist = get_distribution(current_cover, target_dist)
   divergence = kullback_leibler_div(cover_dist, target_dist)
   return divergence
 
@@ -33,6 +38,8 @@ def kullback_leibler_div(dist_1: Dict[Tuple[str, str], float], dist_2: Dict[Tupl
   for value in dist_2.values():
     assert value > 0
   unequal_zero_keys = [key for key in dist_1.keys() if dist_1[key] > 0]
+  if unequal_zero_keys == []:
+    return float('inf')
   divergence = [dist_1[key] * (np.log(dist_1[key]) - np.log(dist_2[key]))
                 for key in unequal_zero_keys]
   return sum(divergence)
@@ -42,8 +49,9 @@ def get_distribution(sentence_list: List[List[Tuple[str, str]]], target_dist: Di
   new_dist = {key: count_occurrences_of_unit_in_list_of_sentences(
     key, sentence_list) for key in target_dist.keys()}
   total_number_of_single_units = sum(new_dist.values())
-  for key, value in new_dist.items():
-    new_dist[key] = value / total_number_of_single_units
+  if total_number_of_single_units != 0:
+    for key, value in new_dist.items():
+      new_dist[key] = value / total_number_of_single_units
   return new_dist
 
 
--- greedy.tests.py
+++ greedy.tests.py
@@ -1,8 +1,9 @@
 import unittest
 
+import numpy as np
+
 from greedy import (count_occurrences_of_unit_in_list_of_sentences,
-                    find_sentence_with_min_div, get_distribution,
-                    get_divergence_for_sentence, greedy)
+                    get_distribution, greedy, kullback_leibler_div)
 
 
 class UnitTests(unittest.TestCase):
@@ -44,7 +45,68 @@ class UnitTests(unittest.TestCase):
 
   # endregion
 
-  # region get_divergence_for_sentence
+  # region kullback_leibler_div
+
+  def test_kullback_leiber(self):
+    dist_1 = {("Hallo", "h"): 1 / 7, ("du", "d"): 3 / 7,
+              ("und", "u"): 2 / 7, ("Bye", "b"): 1 / 7}
+    dist_2 = {("Hallo", "h"): 0.2, ("du", "d"): 0.3, ("und", "u"): 0.4, ("Bye", "b"): 0.1}
+    res = kullback_leibler_div(dist_1, dist_2)
+    right_div = 1 / 7 * np.log((1 / 7) / 0.2) + 3 / 7 * np.log((3 / 7) / 0.3) + \
+        2 / 7 * np.log((2 / 7) / 0.4) + 1 / 7 * np.log((1 / 7) / 0.1)
+
+    self.assertAlmostEqual(right_div, res)
+
+  def test_kullback_leiber__same_dist__expect_zero(self):
+    dist_1 = {("Hallo", "h"): 1 / 7, ("du", "d"): 3 / 7,
+              ("und", "u"): 2 / 7, ("Bye", "b"): 1 / 7}
+    res = kullback_leibler_div(dist_1, dist_1)
+
+    self.assertEqual(0, res)
+
+  # endregion
+
+  # region greedy
+
+  def test_greedy__one_iteration(self):
+    target_dist = {("Hallo", "h"): 0.2, ("du", "d"): 0.3, ("und", "u"): 0.4, ("Bye", "b"): 0.1}
+    selection_list = [[("Hallo", "h"), ("du", "d"), ("und", "u"), ("du", "d")],
+                      [("Bye", "b"), ("und", "u"), ("du", "d")],
+                      [("Irrelevante", "i"), ("Worte", "w")]]
+    res = greedy(target_dist, selection_list, 1)
+    right_cover = [[("Hallo", "h"), ("du", "d"), ("und", "u"), ("du", "d")]]
+
+    self.assertEqual(1, len(res))
+    self.assertEqual(right_cover, res)
+
+  def test_greedy__two_iterations(self):
+    target_dist = {("Hallo", "h"): 0.2, ("du", "d"): 0.3, ("und", "u"): 0.4, ("Bye", "b"): 0.1}
+    selection_list = [[("Hallo", "h"), ("du", "d"), ("und", "u"), ("du", "d")],
+                      [("Bye", "b"), ("und", "u"), ("du", "d")],
+                      [("Irrelevante", "i"), ("Worte", "w")]]
+    res = greedy(target_dist, selection_list, 2)
+    right_cover = [[("Hallo", "h"), ("du", "d"), ("und", "u"), ("du", "d")],
+                   [("Bye", "b"), ("und", "u"), ("du", "d")]]
+
+    self.assertEqual(2, len(res))
+    self.assertEqual(right_cover, res)
+
+  def test_greedy__three_iterations(self):
+    target_dist = {("Hallo", "h"): 0.2, ("du", "d"): 0.3, ("und", "u"): 0.4, ("Bye", "b"): 0.1}
+    selection_list = [[("Hallo", "h"), ("du", "d"), ("und", "u"), ("du", "d")],
+                      [("Bye", "b"), ("und", "u"), ("du", "d")],
+                      [("Irrelevante", "i"), ("Worte", "w")]]
+    res = greedy(target_dist, selection_list, 3)
+    right_cover = [[("Hallo", "h"), ("du", "d"), ("und", "u"), ("du", "d")],
+                   [("Bye", "b"), ("und", "u"), ("du", "d")],
+                   [("Irrelevante", "i"), ("Worte", "w")]]
+
+    self.assertEqual(3, len(res))
+    self.assertEqual(right_cover, res)
+
+  # endregion
+
+
 if __name__ == '__main__':
   suite = unittest.TestLoader().loadTestsFromTestCase(UnitTests)
   unittest.TextTestRunner(verbosity=2).run(suite)
